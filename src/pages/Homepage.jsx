8874a506ada2878b937a599059d7e197f151e89a
slaykalp <sankalp.agrawal2@s.amity.edu>
2024-08-18 03:01:41 +0530

    minor changes

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import About from './About'
+
+const Homepage = () => {
+  return (
+    <div>
+        
+        <h1>Homepage</h1>
+        <About/>
+    </div>
+  )
+}
+
+export default Homepage
